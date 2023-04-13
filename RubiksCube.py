3b04429a9707356ff3b9f7c5b998acc07905f188
Raphi-2Code <70066593+Raphi-2Code@users.noreply.github.com>
2023-04-13 10:31:47 +0200

    Update RubiksCube.py

--- RubiksCube.py
+++ RubiksCube.py
@@ -2,24 +2,29 @@ from ursina import *
 app=Ursina()
 text1=Text(text="Seconds:",y=0.25)
 pl=EditorCamera(position=(-0.081,3.1,-0.7),rotation=(33+1/3,44,0))
+uER1=0
 for x in range(2):
     for z in range(2):
-        exec(f'k{x}{z}=Entity(model="plane",color=color.blue,texture="rubix_cube",x=x,z=z)')
+        uER1+=1
+        exec(f'b{uER1}=Entity(model="plane",color=color.blue,texture="rubix_cube",x=x,z=z)')
 for xd in [0,1]:
     for yd in [-1.5,-0.5]:
-        exec(f'd{str(xd).replace("-","m").replace(".","c")}{str(yd).replace("-","m").replace(".","c")}=Entity(model="plane",color=color.red,texture="rubix_cube",x=xd,y=yd,z=-0.5,rotation_x=90,rotation_y=180)')
+        exec('Entity(model="plane",color=color.red,texture="rubix_cube",x=xd,y=yd,z=-0.5,rotation_x=90,rotation_y=180)')
 for xp in [0,1]:
     for yp in [-1.5,-0.5]:
-        exec(f'p{str(xp).replace("-","m").replace(".","c")}{str(yp).replace("-","m").replace(".","c")}=Entity(model="plane",color=color.orange,texture="rubix_cube",x=xp,y=yp,z=1.5,rotation_x=90)')
+        exec('Entity(model="plane",color=color.orange,texture="rubix_cube",x=xp,y=yp,z=1.5,rotation_x=90)')
 for xl in range(2):
     for zl in range(2):
-        exec(f'l{str(xl).replace("-","m").replace(".","c")}{str(zl).replace("-","m").replace(".","c")}=Entity(model="plane",color=color.green,texture="rubix_cube",x=xl,z=zl,y=-2,rotation_z=180)')
+        exec('Entity(model="plane",color=color.green,texture="rubix_cube",x=xl,z=zl,y=-2,rotation_z=180)')
+uER=0
 for zt in [0,1]:
     for yt in [-1.5,-0.5]:
-        exec(f't{str(yt).replace("-", "m").replace(".", "c")}{str(zt).replace("-", "m").replace(".", "c")}=Entity(model="plane",color=color.yellow,texture="rubix_cube",x=-0.5,z=zt,y=yt,rotation_z=270)')
+        uER+=1
+        exec(f'u{uER}=Entity(model="plane",color=color.yellow,texture="rubix_cube",x=-0.5,z=zt,y=yt,rotation_z=270)')
+
 for zu in [0,1]:
     for yu in [-1.5,-0.5]:
-        exec(f'u{str(yu).replace("-", "m").replace(".", "c")}{str(zu).replace("-", "m").replace(".", "c")}=Entity(model="plane",color=color.white,texture="rubix_cube",x=1.5,z=zu,y=yu,rotation_z=90)')
+        exec(f'Entity(model="plane",color=color.white,texture="rubix_cube",x=1.5,z=zu,y=yu,rotation_z=90)')
 def left():
     kxb = scene.entities[-17].color
     kxt = scene.entities[-19].color
@@ -31,6 +36,11 @@ def left():
     scene.entities[-15].color = scene.entities[-7].color
     scene.entities[-5].color = kxb
     scene.entities[-7].color = kxt
+    pO1 = b1.color
+    b1.color = b2.color
+    b2.color = b3.color
+    b3.color = b4.color
+    b4.color = pO1
 def down():
     kzb=scene.entities[-19].color
     kzt=scene.entities[-20].color
@@ -42,6 +52,11 @@ def down():
     scene.entities[-16].color=scene.entities[-12].color
     scene.entities[-11].color=kzb
     scene.entities[-12].color=kzt
+    pO=u1.color
+    u1.color=u2.color
+    u2.color=u3.color
+    u3.color=u4.color
+    u4.color=pO
 def generatePosition():
     e=["left()","left(),left(),left()","down()","down(),down(),down()"]
     for i in range(10):
@@ -56,7 +71,7 @@ def generatePosition():
                 return "down"
             if dg=="down()":
                 return "up"
-        #print(qg()) <-: That's the step-by-step solution to solve it...
+        #print(qg()) #<-: That's the step-by-step solution to solve it...
 generatePosition()
 timeIt=0
 seconds=0
